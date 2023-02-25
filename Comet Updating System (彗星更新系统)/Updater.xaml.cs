55b428bf639da2f47f66791f02602f86e94127f8
MarsQQ <53840971+MarsQQ@users.noreply.github.com>
2023-02-26 07:50:46 +0800

    Add project files.

---
+++ Comet Updating System (彗星更新系统)/Updater.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Comet_Updating_System__彗星更新系统_
+{
+    /// <summary>
+    /// Interaction logic for Updater.xaml
+    /// </summary>
+    public partial class Updater : Window
+    {
+        public Updater()
+        {
+            InitializeComponent();
+        }
+    }
+}
